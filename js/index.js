32fba5d2adec4fde06900b657a34cf6646846807
adrian zambrano <adrianzamstrong@gmail.com>
2022-04-28 22:54:04 -0400

    array done

--- js/index.js
+++ js/index.js
@@ -66,32 +66,38 @@ class Ropa{
         this.qty = qty;
     }
 }
-const ordenarProductos = []
-
-const producto1 = new Ropa("green polo",8.999);
-const producto2 = new Ropa("black chino pants",22.990);
-const producto3 = new Ropa("regular ragged jeans",32.990);
-const producto4 = new Ropa("printed kimono - blue shirt",18.990);
-const producto5 = new Ropa("t-shirt eagle print",8.999);
-const producto6 = new Ropa("Brown leather jacket men",65.910);
-const producto7 = new Ropa("shiny cat print",8.999);
-const producto8 = new Ropa("Cool sport jacket - orange",42.960);
-const producto9 = new Ropa("Yellow top women oval design",6.999);
-const producto10 = new Ropa("grey fashion pants",9.900);
-const producto11 = new Ropa("color squared coat",35.999);
-const producto12 = new Ropa("sweater rounded neck green",14.990);
-const producto13 = new Ropa("burdeo women windbreaker",25.690);
-const producto14 = new Ropa("blue fashion parka",38.990);
+const ordenarProductos = [
+    new Ropa("green polo",8.999),
+    new Ropa("black chino pants",22.990),
+    new Ropa("regular ragged jeans",32.990),
+    new Ropa("printed kimono - blue shirt",18.990),
+    new Ropa("t-shirt eagle print",8.999),
+    new Ropa("Brown leather jacket men",65.910),
+    new Ropa("shiny cat print",8.999),
+    new Ropa("Cool sport jacket - orange",42.960),
+    new Ropa("Yellow top women oval design",6.999),
+    new Ropa("grey fashion pants",9.900),
+    new Ropa("color squared coat",35.999),
+    new Ropa("sweater rounded neck green",14.990),
+    new Ropa("burdeo women windbreaker",25.690),
+    new Ropa("blue fashion parka",38.990)
+]
 
 /*
     ----------------------------------------
     |              BUSCADOR                |
     ----------------------------------------
 */
+const searchingBarContent = document.querySelector("#search__input").value;
+let lense = document.getElementById("lupa");
 
-// let buscar = numeros.find(obj => {
-//     return obj.nombre == search
-// });
+// function buscar(e){
+//     ordenarProductos.find(obj)
+// };
+
+// lense.addEventListener("click", buscar(searchingBarContent));
+// indexOf !
+// 
 
 // let filtrarPorPrecio = numeros.filter(obj => 
 //     {return obj.precio <= 1000});
