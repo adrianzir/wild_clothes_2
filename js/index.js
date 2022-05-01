7b76a928e12e2235b7fd156bc4cb724aca48a776
adrian zambrano <adrianzamstrong@gmail.com>
2022-05-01 00:51:15 -0400

    javascript search and arrays started

--- js/index.js
+++ js/index.js
@@ -35,16 +35,35 @@ const email = document.querySelector(".email");
 const age = document.querySelector(".age");
 const submit = document.querySelector("#send");
 /*
-    ----------------------------------------
+
     |      FUNCIONES PARA EL FORMULARIO    |
-    ----------------------------------------
 */
+
 function nameValidation(){
     nombre.addEventListener("input", ()=>{
-    if(nombre.value == ""){
-        nombre.classList("emptyInput");
+        if(nombre.value === ""){
+            nombre.classList.toggle("emptyInput");
+              Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Este campo debe ser llenado'
+                })
+        }else{
+            nombre.classList.toggle("inputAccepted");
+        }
+    })
+}
+function emailValidation(){
+    email.addEventListener("input", ()=>{
+    if(email.value === ""){
+        email.classList.toggle("emptyInput");
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Este campo debe ser llenado'
+            })
     }else {
-        nombre.classList ="inputaccepted";
+        email.classList.toggle("inputAccepted");
     }
 })
 }
@@ -52,7 +71,12 @@ function nameValidation(){
 submit.addEventListener("click", (e)=>{  // <-------------------EVENTO
     e.preventDefault();
     nameValidation();
-    alert(`bienvenido ${nombre.value}, ahora recibiras notificaciones de nuestras actualizaciones, ofertas, productos y demas, al correo: ${email.value}`)
+    emailValidation();
+    if(nombre.value, email.value != ""){
+        Swal.fire(`Bienvenido ${nombre.value}, ahora recibiras notificaciones de nuestras actualizaciones, ofertas, productos y demas, al correo: ${email.value}`)
+    }else{
+        
+    }
 })
 /*
     ----------------------------------------
@@ -66,39 +90,57 @@ class Ropa{
         this.qty = qty;
     }
 }
-const ordenarProductos = [
-    new Ropa("green polo",8.999),
-    new Ropa("black chino pants",22.990),
-    new Ropa("regular ragged jeans",32.990),
-    new Ropa("printed kimono - blue shirt",18.990),
-    new Ropa("t-shirt eagle print",8.999),
-    new Ropa("Brown leather jacket men",65.910),
-    new Ropa("shiny cat print",8.999),
-    new Ropa("Cool sport jacket - orange",42.960),
-    new Ropa("Yellow top women oval design",6.999),
-    new Ropa("grey fashion pants",9.900),
-    new Ropa("color squared coat",35.999),
-    new Ropa("sweater rounded neck green",14.990),
-    new Ropa("burdeo women windbreaker",25.690),
-    new Ropa("blue fashion parka",38.990)
-]
+// const ordenarProductos = [
+//     new Ropa("green polo",8.999),
+//     new Ropa("black chino pants",22.990),
+//     new Ropa("regular ragged jeans",32.990),
+//     new Ropa("printed kimono - blue shirt",18.990),
+//     new Ropa("t-shirt eagle print",8.999),
+//     new Ropa("Brown leather jacket men",65.910),
+//     new Ropa("shiny cat print",8.999),
+//     new Ropa("Cool sport jacket - orange",42.960),
+//     new Ropa("Yellow top women oval design",6.999),
+//     new Ropa("grey fashion pants",9.900),
+//     new Ropa("color squared coat",35.999),
+//     new Ropa("sweater rounded neck green",14.990),
+//     new Ropa("burdeo women windbreaker",25.690),
+//     new Ropa("blue fashion parka",38.990)
+// ]
+const producto1 = document.querySelector("#product-1");
+const producto2 = document.querySelector("#product-2");
+const producto3 = document.querySelector("#product-3");
+const producto4 = document.querySelector("#product-4");
+const producto5 = document.querySelector("#product-5");
+const producto6 = document.querySelector("#product-6");
+const producto7 = document.querySelector("#product-7");
+const producto8 = document.querySelector("#orange-j");
+const producto9 = document.querySelector("#product-9");
+const producto10 = document.querySelector("#product-10");
+const producto11 = document.querySelector("#product-11");
+const producto12 = document.querySelector("#green_sweater");
+const producto13 = document.querySelector("#product-13");
+const producto14 = document.querySelector("#product-14");
 
-/*
+const productos2 = []
+
+/*             
     ----------------------------------------
     |              BUSCADOR                |
     ----------------------------------------
 */
-const searchingBarContent = document.querySelector("#search__input").value;
-let lense = document.getElementById("lupa");
+// const searchingBarContent = document.querySelector("#search__input");
+// let lense = document.getElementById("lupa");
 
-// function buscar(e){
-//     ordenarProductos.find(obj)
-// };
 
-// lense.addEventListener("click", buscar(searchingBarContent));
-// indexOf !
-// 
 
+// lense.addEventListener("click",);
+// // indexOf !
+// // 
+/*
+    ----------------------------------------
+    |              FILTROS                 |
+    ----------------------------------------
+*/
 // let filtrarPorPrecio = numeros.filter(obj => 
 //     {return obj.precio <= 1000});
 // console.log(filtrarPorPrecio)
